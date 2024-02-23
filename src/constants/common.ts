298b7b8dd46f55935acc2bdc821300e0ca908728
ThangDevAlone <quangthangvtlg@gmail.com>
2024-02-24 06:31:29 +0700

    hehe

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 // export const STATIC_HOST = "https://server-iot-food-04.onrender.com/"
-export const STATIC_HOST = "https://ai.thangdev.online/"
-export const SOCKET_URL = `ws://ai.thangdev.online/ws/`
+export const STATIC_HOST = "https://api.thangdev.online/"
+export const SOCKET_URL = `ws://api.thangdev.online/ws/`
